65f6024e2fc69966602a5c22ee27a80742c44e07
enjoyurlove <enjoyurlove@3bde79f6-0018-11de-99c0-75d572ba1ddd>
2009-03-10 18:54:15 +0000

    Start to develop CodeConverter

---
+++ NEnhancer/CodeConverter.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace NEnhancer
+{
+    partial class CodeConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(23, 22);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 12);
+            this.lblMessage.TabIndex = 0;
+            // 
+            // CodeConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 412);
+            this.Controls.Add(this.lblMessage);
+            this.Name = "CodeConverter";
+            this.Text = "CodeConverter";
+            this.Load += new System.EventHandler(this.CodeConverter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
---
+++ NEnhancer/CodeConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NEnhancer
+{
+    public partial class CodeConverter : AddInForm
+    {
+        public CodeConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void CodeConverter_Load(object sender, EventArgs e)
+        {
+            if (!HasSolution)
+            {
+                lblMessage.Text = "Solution not opened";
+            }
+        }
+
+        private bool HasSolution
+        {
+            get
+            {
+                return DTEObject.Solution.IsOpen;
+            }
+        }
+    }
+}
