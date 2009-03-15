8dff585a87b8f65f9a885d92d587890a1b1a60be
enjoyurlove <enjoyurlove@3bde79f6-0018-11de-99c0-75d572ba1ddd>
2009-03-15 18:31:43 +0000

    Add NPetshopSlnGenerator Command

---
+++ NEnhancer/NPetshopSlnGenerator.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace NEnhancer
+{
+    partial class NPetshopSlnGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSlnName = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Solution Name:";
+            // 
+            // txtSlnName
+            // 
+            this.txtSlnName.Location = new System.Drawing.Point(107, 13);
+            this.txtSlnName.Name = "txtSlnName";
+            this.txtSlnName.Size = new System.Drawing.Size(270, 21);
+            this.txtSlnName.TabIndex = 1;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(14, 48);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Message:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(14, 99);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(363, 273);
+            this.textBox2.TabIndex = 4;
+            // 
+            // NPetshopSlnGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 387);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtSlnName);
+            this.Controls.Add(this.label1);
+            this.Name = "NPetshopSlnGenerator";
+            this.Text = "Generate A New NPetshop-Style Solution";
+            this.Load += new System.EventHandler(this.NPetshopSlnGenerater_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSlnName;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ NEnhancer/NPetshopSlnGenerator.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+using EnvDTE;
+using EnvDTE80;
+using EnvDTE90;
+using VSLangProj;
+
+namespace NEnhancer
+{
+    public partial class NPetshopSlnGenerator : AddInForm
+    {
+        #region Constants
+
+        private const string ExternalBinDirectoryName = "External-bin";
+
+        #endregion
+
+        private string currentSlnPath = string.Empty;
+
+        private string ExternalBinPath
+        {
+            get
+            {
+                return Path.Combine(currentSlnPath, ExternalBinDirectoryName);
+            }
+        }
+
+        public NPetshopSlnGenerator()
+        {
+            InitializeComponent();
+        }
+
+        private void NPetshopSlnGenerater_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            if (!ValidateCtrls())
+            {
+                return;
+            }
+
+            try
+            {
+                FolderBrowserDialog dlgFolder = new FolderBrowserDialog();
+                if (dlgFolder.ShowDialog() == DialogResult.OK)
+                {
+                    currentSlnPath = dlgFolder.SelectedPath;
+                    Generate(txtSlnName.Text.Trim());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error occured: " + ex.Message);
+            }
+        }
+
+        private void Generate(string slnName)
+        {
+            Solution3 sln = (DTEObject.Solution as Solution3);
+            sln.Create(currentSlnPath, slnName);
+
+            CopyExternalBinFolder(currentSlnPath);
+
+            // Create solution folder
+            Project sfProj = sln.AddSolutionFolder(ExternalBinDirectoryName);
+            foreach (string file in Directory.GetFiles(ExternalBinPath))
+            {
+                sfProj.ProjectItems.AddFromFile(file);
+            }
+
+            // Create *.Domain Project
+            string classLibProjTemplatePath =
+                sln.GetProjectTemplate("ClassLibrary.zip", "CSharp");
+            string domainProjName = slnName + "." + "Domain";
+            sln.AddFromTemplate(classLibProjTemplatePath, Path.Combine(currentSlnPath, domainProjName),
+                domainProjName, false);
+            Project domainProj = GetProjectByName(sln, domainProjName);
+            VSProject vsDomainProj = domainProj.Object as VSProject;
+            vsDomainProj.References.Add(Path.Combine(ExternalBinPath, "IBatisNet.Common.dll"));
+
+            // Create *.Persistence Project
+            string persistProjName = slnName + "." + "Persistence";
+            sln.AddFromTemplate(classLibProjTemplatePath, Path.Combine(currentSlnPath, persistProjName),
+                persistProjName, false);
+            Project persistProj = GetProjectByName(sln, persistProjName);
+            VSProject vsPersistProj = persistProj.Object as VSProject;
+            vsPersistProj.References.Add(Path.Combine(ExternalBinPath, "IBatisNet.Common.dll"));
+            vsPersistProj.References.Add(Path.Combine(ExternalBinPath, "IBatisNet.DataAccess.dll"));
+            vsPersistProj.References.Add(Path.Combine(ExternalBinPath, "IBatisNet.DataMapper.dll"));
+            vsPersistProj.References.AddProject(domainProj);
+
+            // Create *.Service Project
+            string serviceProjName = slnName + "." + "Service";
+            sln.AddFromTemplate(classLibProjTemplatePath, Path.Combine(currentSlnPath, serviceProjName),
+                serviceProjName, false);
+            Project serviceProj = GetProjectByName(sln, serviceProjName);
+            VSProject vsServiceProj = serviceProj.Object as VSProject;
+            vsServiceProj.References.Add(Path.Combine(ExternalBinPath, "IBatisNet.Common.dll"));
+            vsServiceProj.References.Add(Path.Combine(ExternalBinPath, "IBatisNet.DataAccess.dll"));
+            vsServiceProj.References.Add(Path.Combine(ExternalBinPath, "Castle.DynamicProxy.dll"));
+            vsServiceProj.References.Add(Path.Combine(ExternalBinPath, "log4net.dll"));
+            vsServiceProj.References.AddProject(domainProj);
+            vsServiceProj.References.AddProject(persistProj);
+
+            // Create *.Presentation Project
+            string presentProjName = slnName + "." + "Presentation";
+            sln.AddFromTemplate(classLibProjTemplatePath, Path.Combine(currentSlnPath, presentProjName),
+                presentProjName, false);
+            Project presentProj = GetProjectByName(sln, presentProjName);
+            VSProject vsPresentProj = presentProj.Object as VSProject;
+            vsPresentProj.References.Add(Path.Combine(ExternalBinPath, "IBatisNet.Common.dll"));
+            vsPresentProj.References.AddProject(domainProj);
+            vsPresentProj.References.AddProject(serviceProj);
+
+            // Create *.Web Project
+            string webProjTemplatePath =
+                sln.GetProjectTemplate("WebApplicationProject.zip", "CSharp");
+            string webProjName = slnName + "." + "Web";
+            sln.AddFromTemplate(webProjTemplatePath, Path.Combine(currentSlnPath, webProjName),
+                webProjName, false);
+            Project webProj = GetProjectByName(sln, webProjName);
+            VSProject vsWebProj = webProj.Object as VSProject;
+            vsWebProj.References.Add(Path.Combine(ExternalBinPath, "Castle.DynamicProxy.dll"));
+            vsWebProj.References.Add(Path.Combine(ExternalBinPath, "IBatisNet.Common.dll"));
+            vsWebProj.References.Add(Path.Combine(ExternalBinPath, "IBatisNet.Common.Logging.Log4Net.dll"));
+            vsWebProj.References.Add(Path.Combine(ExternalBinPath, "log4net.dll"));
+            vsWebProj.References.AddProject(domainProj);
+            vsWebProj.References.AddProject(presentProj);
+
+            webProj.ProjectItems.AddFolder("css", Constants.vsProjectItemKindPhysicalFolder);
+            webProj.ProjectItems.AddFolder("images", Constants.vsProjectItemKindPhysicalFolder);
+            webProj.ProjectItems.AddFolder("Maps", Constants.vsProjectItemKindPhysicalFolder);
+            webProj.ProjectItems.AddFolder("UserControls", Constants.vsProjectItemKindPhysicalFolder);
+
+            webProj.ProjectItems.AddFromFileCopy(
+                Path.Combine(GetAddinPath(), @"ibatis-config\dao.config"));
+            webProj.ProjectItems.AddFromFileCopy(
+                Path.Combine(GetAddinPath(), @"ibatis-config\providers.config"));
+            webProj.ProjectItems.AddFromFileCopy(
+                Path.Combine(GetAddinPath(), @"ibatis-config\SqlMap.config"));
+
+            sln.SaveAs(slnName);
+        }
+
+        private void CopyExternalBinFolder(string slnPath)
+        {
+            string binDir = Path.Combine(GetAddinPath(), "ibatis-bin");
+            string targetDir = ExternalBinPath;
+            if (!Directory.Exists(targetDir))
+            {
+                Directory.CreateDirectory(targetDir);
+            }
+
+            // Copy files
+            foreach (string file in Directory.GetFiles(binDir))
+            {
+                string fileName = Path.GetFileName(file);
+                File.Copy(file, Path.Combine(targetDir, fileName), true);
+            }
+        }
+
+        private bool ValidateCtrls()
+        {
+            if (string.IsNullOrEmpty(txtSlnName.Text.Trim())) { return false; }
+
+            return true;
+        }
+
+        // TODO: Move to helper class.
+        private string GetAddinPath()
+        {
+            return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        }
+
+        // TODO: Move to helper class.
+        private Project GetProjectByName(Solution2 sln, string projName)
+        {
+            foreach (Project p in sln.Projects)
+            {
+                if (p.Name == projName)
+                {
+                    return p;
+                }
+            }
+
+            return null;
+        }
+    }
+}
